f36b4b39ecbc8dd31da3650f367a401f1ecb30df
Abby Blubaugh <abigailrblubaugh@gmail.com>
2019-06-07 20:51:20 -0700

    set up

---
+++ src/components/Game/game.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class Game extends Component {
+    // Setting the component's initial state
+    state = {
+    //   firstName: "",
+    //   lastName: "",
+    //   password: ""
+    // };
